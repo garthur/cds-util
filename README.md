194927c27eaa538fe471817295e169eab1a21228
Graham Arthur <grahamarthur@Grahams-MacBook-Pro.local>
2022-02-12 20:51:16 -0500

    initial commit of era5 utils

---
+++ README.md
@@ -0,0 +1,9 @@
+# CDS Utils
+
+Utilities for climate data science.
+
+## ERA5 CLI
+
+Example Uses:
+`cds-era --dataset reanalysis-era5-single-levels --var 2m_temperature --dates 1996-05-31 --grid 0.25 0.25 --area 13.55 -59.90 12.82 -59.20 --outfile ./brb.nc`
+`cds-era --dataset reanalysis-era5-single-levels-monthly-means --var 2m_temperature --dates 2021-02-01 --grid 0.25 0.25 --dry_run`
---
+++ setup.py
@@ -0,0 +1,29 @@
+
+from setuptools import setup, find_packages
+  
+with open('requirements.txt') as f:
+    requirements = f.readlines()
+
+  
+setup(
+        name ='cds_util',
+        version ='1.0.0',
+        author ='Graham Arthur',
+        author_email ='graham.d.arthur18@gmail.com',
+        description ='Climate Data Science util package.',
+        license ='MIT',
+        packages = find_packages(),
+        entry_points ={
+            'console_scripts': [
+                'cds-era5 = cds_util.era5:main'
+            ]
+        },
+        classifiers =(
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+        ),
+        keywords="cds",
+        install_requires = requirements,
+        zip_safe = False
+)
